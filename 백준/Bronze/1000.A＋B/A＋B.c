efc7dd56a184d27417ef497e00261332f027b433
minina0407 <minina@kyonggi.ac.kr>
2022-01-21 09:56:29 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1000.A＋B/A＋B.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ int main(void)
+ {
+     int x;
+     int y;
+     int sum;
+     
+     x=1;
+     y=2;
+     sum=x+y;
+     
+    scanf("%d %d",&x,&y);
+	printf("%d\n",x+y);
+         
+     return 0 ;
+ }
