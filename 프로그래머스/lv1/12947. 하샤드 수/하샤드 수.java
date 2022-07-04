b4acf9cf1dd165d526d12fcfeca219b22b10c4f5
minina <minina@kyonggi.ac.kr>
2022-07-05 00:29:04 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean solution(int x) {
+
+        boolean answer =true;
+
+     
+        int sum = 0;
+        int a =x;
+        
+        while(x!=0){
+        
+            sum += x%10;
+            x /= 10;
+        }
+        if(a % sum == 0)
+            return answer;
+        return false;    }
+}
