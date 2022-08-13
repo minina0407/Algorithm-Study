86c41b61fdb8f3273a065a182ccd431b692a2ab9
minina <minina@kyonggi.ac.kr>
2022-08-13 20:19:23 +0900

    [level 1] Title: K번째수, Time: 0.32 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -6,12 +6,15 @@ class Solution {
             int start = commands[i][0];
             int end = commands[i][1];
             int select = commands[i][2];
+            // 사이즈는 끝 - 시작  + 1
             int[] newarray = new int[end-start+1];
             int idx=0;
             for(int j = start-1;j<end;j++){
+                // 배열에 담기 (배열은 0부터 시작이라 start-1 해줌 )
                 newarray[idx]  = array[j];
                 idx++;
             }
+            // 정렬
             Arrays.sort(newarray);
             answer[i] = newarray[select-1];
         }
