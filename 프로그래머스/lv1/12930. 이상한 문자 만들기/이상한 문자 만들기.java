7ae3fdd65a6e769e1bf5de0bb09f7b1cfb9e2a1a
minina <minina@kyonggi.ac.kr>
2022-08-21 18:06:11 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.33 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -4,9 +4,12 @@
         String[] array = s.split("");
         int count = 0;
         for(int i = 0 ; i<array.length;i++){
+            
             if(array[i].equals(" ")){
+                //띄어쓰기 단위로 count 초기화하기 ->왜? 단어 단위로 짝,홀수 판단 해야되기때문에
                 count = 0;
             }
+            
          else if(count %2 == 0){
              array[i] = array[i].toUpperCase();
              count++;
