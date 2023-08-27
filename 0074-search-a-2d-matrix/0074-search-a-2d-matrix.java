71ccf9c79cbc3c7d278ca6916e2ab11485241663
minina <minina1868@gmail.com>
2023-08-27 11:19:41 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (7.32%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        boolean isTrue = false;
-        for(int i = 0 ;i < matrix.length; i++){
-            for(int j= 0 ; j<matrix[i].length; j++){
-                if(matrix[i][j]==target){
-                    isTrue = true;
-                }
-            }
-        }
-        if (matrix.length == 0) {
-            return false;
+        int left = 0;
+        int right =  matrix.length * matrix[0].length -1;
+        
+        while(left <= right){
+            int mid = (left + right) / 2;
+            
+            // 중간 위치의 행열 구하기
+            int midVal = matrix[mid/matrix[0].length][mid%matrix[0].length];
+            
+            if(midVal == target) return true;
+            else if(midVal < target) left = mid + 1;
+            else right = mid - 1;
         }
         
-        return isTrue;
+        return false;
     }
 }
