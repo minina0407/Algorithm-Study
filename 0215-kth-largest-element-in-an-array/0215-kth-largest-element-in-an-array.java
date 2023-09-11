a8ad7e063a95de9aaac17926a4af023a6275b0f9
minina <minina1868@gmail.com>
2023-09-11 15:41:26 +0900

    Time: 59 ms (21.73%), Space: 57.6 MB (5.6%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -0,0 +1,21 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if (nums.length == 0 || k == 0) {
+            return 0; 
+        }
+        
+        // 우선순위 큐 ( 내림차순 )
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> a - b);
+
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll(); // 큐의 크기가 k를 초과하면 가장 작은 요소 제거
+            }
+        }
+
+        return minHeap.poll(); // k번째로 큰 요소 반환
+    }
+}
