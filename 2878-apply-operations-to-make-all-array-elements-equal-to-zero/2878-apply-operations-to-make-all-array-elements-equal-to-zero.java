f0db5259cdf1e9d999d72620789775318ab96b53
minina <minina1868@gmail.com>
2023-10-07 14:19:45 +0900

    Time: 1 ms (100%), Space: 58.2 MB (49.53%) - LeetHub

--- 2878-apply-operations-to-make-all-array-elements-equal-to-zero/2878-apply-operations-to-make-all-array-elements-equal-to-zero.java
+++ 2878-apply-operations-to-make-all-array-elements-equal-to-zero/2878-apply-operations-to-make-all-array-elements-equal-to-zero.java
@@ -1,16 +1,19 @@
 class Solution {
-    public boolean checkArray(int[] nums, int k) {
-        int cur = 0 ; // 현재 subarray의 합
+ public boolean checkArray(int[] nums, int k) {
+    int cur = 0; // 현재 subarray의 합
+    
+    for (int i = 0; i < nums.length; i++) {
+        if (cur > nums[i])
+            return false; // 현재 subarray의 합이 현재 요소보다 크면 false 반환
         
-        for(int i = 0 ; i <nums.length ;i++){
-            if(cur > nums[i])
-                 return false;
-            // k크기의 subarray 찾기
-            nums[i] -=cur;
-            cur+=nums[i];
-            if(i >= k-1) // 현재 subarray의 크기가 k일때
-            cur-=nums[i-k+1]; // subarray의 첫번째 요소를 제거
-        }
-        return cur ==0;
+        nums[i] -= cur; // 현재 요소에서 cur 값을 빼서 현재 subarray의 첫 번째 요소를 0으로 만듦
+        cur += nums[i]; // cur에 현재 요소 값을 더하여 현재 subarray의 합 업데이트
+        
+        if (i >= k - 1)
+            cur -= nums[i - k + 1]; // 현재 subarray의 크기가 k일 때, subarray의 첫 번째 요소를 제거
     }
+    
+    return cur == 0; // 모든 요소를 처리한 후, cur 값이 0이면 true 반환
+}
+
 }
