4bb0989a42ae786b4bf59edfd304d9a3e3a35dcf
minina <minina1868@gmail.com>
2023-11-17 23:01:31 +0900

    Time: 34 ms (81.13%), Space: 44.4 MB (88.68%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -3,13 +3,13 @@ class Solution {
      StringBuilder sb = new StringBuilder();
         for(int i = 0 ; i<s.length() ; i++){
             if(s.charAt(i) == '*'){
-                 if(sb.length() == 0){
-                    continue;
-                }
+                // * 의 바로 왼쪽 문자 삭제 
                 sb.deleteCharAt(sb.length()-1);
                 continue;
             }
+            
             sb.append(s.charAt(i));
+
         }
         return sb.toString();
     }
