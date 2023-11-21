64332288e28b1932119bb0ba6f236bd64979ed3d
minina <minina1868@gmail.com>
2023-11-21 17:18:12 +0900

    Time: 60 ms (59.59%), Space: 55.8 MB (57.29%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -5,6 +5,9 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
       
        for(int i = 0 ; i<temperatures.length ; i++){
+                 // 스택이 비어있지않고 현재 온도가 스택의 top보다 높을 경우
+           // 스택의 top에 해당하는 인덱스를 pop하고
+              // 현재 인덱스와 pop한 인덱스의 차이를 waitDay에 넣어준다.
             while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]){
                int index = stack.pop();
               waitDay[index] = i - index;
